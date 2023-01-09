62c95d129ca07b3692f4232c970a74050bf2dee0
t3chnologist <batnomin.terbish@gmail.com>
2023-01-09 16:41:47 -0700

    Updated studentData.

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main()
 		"A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
 		"A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
 		"A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-		"A5,Batnomin,Terbish,batnomin@wgu.edu,31,34,34,55,SOFTWARE" //Included personal info in studentData[];
+		"A5,Batnomin,Terbish,batnomin@wgu.edu,31,34,34,55,SOFTWARE" 
 	};
 
 	cout << "C867-Scripting & Programming: Applications\nLanguage: C++\nStudent ID:0011223344\nName: Batnomin Terbish\n\n";
